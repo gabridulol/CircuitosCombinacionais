fb77654d9bb3dbb41824b87bd849d2a4692931c2
Gabriel R. Marques <gabriel.r.marques@ufv.br>
2023-10-08 01:01:12 -0300

    a

--- Verilog/Display.v
+++ Verilog/Display.v
@@ -3,12 +3,12 @@ module Display (
     output [6:0] Display
 );
 
-assign Display[6] = ~S2 ~S0 + ~S3 S1 + ~S3 S2 S0 + S2 S1 + S3 ~S2 ~S1 + S3 ~S0
-assign Display[5] = ~S3 ~S2 + ~S3 ~S1 ~S0 + ~S2 ~S0 + ~S3 S1 S0 + S3 ~S1 S0
-assign Display[4] = ~S3 ~S1 + ~S3 S0 + ~S1 S0 + ~S3 S2 + S3 ~S2
-assign Display[3] = ~S3 ~S2 ~S0 + ~S2 S1 S0 + S2 ~S1 S0 + S2 S1 ~S0 + S3 ~S1
-assign Display[2] = ~S2 ~S0 + S1 ~S0 + S3 S1 + S3 S2
-assign Display[1] = ~S1 ~S0 + ~S3 S2 ~S1 + S2 ~S0 + S3 ~S2 + S3 S1
-assign Display[0] = ~S2 S1 + S2 ~S1 + S2 ~S0 + S3
+assign Display[6] = (~Output[2] & ~Output[0]) | (~Output[3] & Output[1]) | (~Output[3] & Output[2] & Output[0]) | (Output[2] & Output[1]) | (Output[3] & ~Output[2] & ~Output[1]) | (Output[3] & ~Output[0]);
+assign Display[5] = (~Output[3] & ~Output[2]) | (~Output[3] & ~Output[1] & ~Output[0]) | (~Output[2] & ~Output[0]) | (~Output[3] & Output[1] & Output[0]) | (Output[3] & ~Output[1] & Output[0]);
+assign Display[4] = (~Output[3] & ~Output[1]) | (~Output[3] & Output[0]) | (~Output[1] & Output[0]) | (~Output[3] & Output[2]) | (Output[3] & ~Output[2]);
+assign Display[3] = (~Output[3] & ~Output[2] & ~Output[0]) | (~Output[2] & Output[1] & Output[0]) | (Output[2] & ~Output[1] & Output[0]) | (Output[2] & Output[1] & ~Output[0]) | (Output[3] & ~Output[1]);
+assign Display[2] = (~Output[2] & ~Output[0]) | (Output[1] & ~Output[0]) | (Output[3] & Output[1]) | (Output[3] & Output[2]);
+assign Display[1] = (~Output[1] & ~Output[0]) | (~Output[3] & Output[2] & ~Output[1]) | (Output[2] & ~Output[0]) | (Output[3] & ~Output[2]) | (Output[3] & Output[1]);
+assign Display[0] = (~Output[2] & Output[1]) | (Output[2] & ~Output[1]) | (Output[2] & ~Output[0]) | (Output[3]);
 
 endmodule
--- Verilog/Testbench.v
+++ Verilog/Testbench.v
@@ -1,10 +1,10 @@
-module Codificador_tb;
+module Testbench();
 
   reg [3:0] Input;
   reg Ready;
   reg Reset;
   wire [3:0] Output;
-  wire [7:0] Display;
+  wire [6:0] Display;
 
   Codificador UUT_Codificador (
     .Input(Input),
@@ -16,10 +16,23 @@ module Codificador_tb;
   Display UUT_Display (
     .Output(Output),
     .Display(Display)
-  )
+  );
+
+  integer i;
 
   initial begin
-    
+    $dumpfile("GTKWaves.vcd");
+    $dumpvars();
+    Ready = 0; // Sinal de Controle - Ready
+    Reset = 0; // Sinal de Controle - Reset
+    for (i = 0; i <= 15; i = i + 1) begin
+      Input = i;
+      #10
+      $display("Entrada : %b %b %b %b", Input[3], Input[2], Input[1], Input[0]);
+      $display("Codificado : %b %b %b %b", Output[3], Output[2], Output[1], Output[0]);
+      $display("Display : %b %b %b %b %b %b %b\n", Display[6], Display[5], Display[4], Display[3], Display[2], Display[1], Display[0]);
+    end
+    $finish;
   end
 
 endmodule
